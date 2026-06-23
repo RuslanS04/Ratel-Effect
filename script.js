f03ab7b1e14713b58e4f282d3f2bb232bb6ea66a
RuslanS04 <ruslan72810407@gmail.com>
2026-06-23 23:21:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const CONFIG = {
   CHAIN_EXPLORER:           'https://basescan.org',
 
   // ── Mint Parameters ────────────────────────────────────────
-  MINT_PRICE:               0.000573,   // ETH 0.000573
+  MINT_PRICE:               0.00072,   // ETH 0.000573
   MAX_PER_TX:               100,
 
   // ── Timer / Countdown ──────────────────────────────────────
